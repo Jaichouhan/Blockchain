eedf4d0d3088206696d489befd447b8a516c1241
Jai chouhan <92376924+Jaichouhan@users.noreply.github.com>
2022-10-06 16:19:41 +0530

    blockchain

---
+++ dev/blockchain.js
@@ -0,0 +1,50 @@
+function Blockchain() {
+  this.chain = [];
+  this.pendingTransaction = [];
+}
+
+// class Blockchain {
+//   constructor() {
+//     this.chain = [];
+//     this.pendingTransaction = {};
+//   }
+// }
+
+Blockchain.prototype.createNewBlock = function (
+  nonce,
+  previousBlockHash,
+  hash
+) {
+  const newBlock = {
+    index: this.chain.length + 1,
+    timestamp: Date.now(),
+    transactions: this.pendingTransaction,
+    nonce: nonce,
+    hash: hash,
+    previousBlockHash: previousBlockHash,
+  };
+  this.pendingTransaction = [];
+  this.chain.push(newBlock);
+
+  return newBlock;
+};
+
+Blockchain.prototype.getLastBlock = function () {
+  return this.chain[this.chain.length - 1];
+};
+
+Blockchain.prototype.createNewTranscation = function (
+  amount,
+  sender,
+  recipient
+) {
+  const newTransaction = {
+    amount: amount,
+    sender: sender,
+    recipient: recipient,
+  };
+  this.pendingTransaction.push(newTransaction);
+  return this.getLastBlock()["index"] + 1;
+};
+
+module.exports = Blockchain;
---
+++ dev/test.js
@@ -0,0 +1,12 @@
+const Blockchain = require("./blockchain");
+
+const bitCoin = new Blockchain();
+bitCoin.createNewBlock(2701, "MJ2701", "JAIMAHAK");
+bitCoin.createNewTranscation(323245, "JAI88938BJKBJ", "MAHAK8367JNJKBF");
+bitCoin.createNewBlock(0304, "MJ0304", "JANAK");
+bitCoin.createNewTranscation(300, "JAI88938BJKBJ", "MAHAK8367JNJKBF");
+bitCoin.createNewTranscation(500, "JAI88938BJKBJ", "MAHAK8367JNJKBF");
+bitCoin.createNewTranscation(400, "JAI88938BJKBJ", "MAHAK8367JNJKBF");
+bitCoin.createNewBlock(0304, "MJ0304", "JANAK");
+
+console.log(bitCoin.chain[2]);

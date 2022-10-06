6de30bad26504a279931525f8cebccdc8fe32fab
Jai chouhan <92376924+Jaichouhan@users.noreply.github.com>
2022-10-06 16:21:03 +0530

    comment

--- dev/test.js
+++ dev/test.js
@@ -9,4 +9,4 @@ bitCoin.createNewTranscation(500, "JAI88938BJKBJ", "MAHAK8367JNJKBF");
 bitCoin.createNewTranscation(400, "JAI88938BJKBJ", "MAHAK8367JNJKBF");
 bitCoin.createNewBlock(0304, "MJ0304", "JANAK");
 
-console.log(bitCoin.chain[0]);
+// console.log(bitCoin.chain[0]);

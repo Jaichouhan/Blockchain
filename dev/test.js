353f6cea0bae1c010327cbecb562622a1caf6454
Jai chouhan <92376924+Jaichouhan@users.noreply.github.com>
2022-10-07 13:02:10 +0530

    created hash block

--- dev/test.js
+++ dev/test.js
@@ -9,4 +9,26 @@ bitCoin.createNewTranscation(500, "JAI88938BJKBJ", "MAHAK8367JNJKBF");
 bitCoin.createNewTranscation(400, "JAI88938BJKBJ", "MAHAK8367JNJKBF");
 bitCoin.createNewBlock(0304, "MJ0304", "JANAK");
 
-console.log(bitCoin.chain[0]);
+const currentBlockData = [
+  {
+    nonce: 2701,
+    hash: "JAIMAHAK",
+    previousBlockHash: "MJ2701",
+  },
+  {
+    nonce: 68687,
+    hash: "BEJWJBE98F7F98EWBF",
+    previousBlockHash: "BIHDF74664GH",
+  },
+  {
+    nonce: 65767,
+    hash: "BHFBD87RDFG5",
+    previousBlockHash: "VJHFHBGI67352G",
+  },
+];
+const previousBlockHash = "JKBVFYEER8T7V8VGEGV";
+const nonce = 89787;
+
+console.log(bitCoin.hashBlock(previousBlockHash, currentBlockData, nonce));
+
+// console.log(bitCoin.chain[0]);
